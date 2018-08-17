1cafffe3864ddc9cfd631360680d4fe35768644f
Matteo Maronati <matteo.maronati@flixbus.com>
2018-08-17 16:53:43 +0200

    Edit: sr for woof routes

--- routes/woof.js
+++ routes/woof.js
@@ -2,52 +2,54 @@ const { readFileSync, writeFileSync, unlinkSync, existsSync, mkdirSync } = requi
 const { resolve } = require('path')
 const puppeteer = require('puppeteer')
 const handlebars = require('handlebars')
-
+const env = require('../env')
 const woof = (app, storageDir) => {
   app.get('/woof/:template.svg', async function (req, res) {
     let urlHash = await generateHash(req.url)
     let targetDir = `${storageDir}/${req.params.template}`
-    if (existsSync(`${targetDir}/${urlHash}.svg`)) {
+    if (existsSync(`${targetDir}/${urlHash}.svg`) && !env.forceOverwite) {
       res.sendFile(resolve(`${targetDir}/${urlHash}.svg`))
       return false
     }
     let hbsData = req.query
     // meh
     hbsData.baseUrl = req.headers.host
-    let htmlPath = await hbsTemplateToHtml(resolve(`./templates/${req.params.template}.hbs`), hbsData, storageDir)
+    let html = await hbsTemplateToHtml(resolve(`./templates/${req.params.template}.hbs`), hbsData)
+    let htmlPath = `${targetDir}/${urlHash}.html`
+    await writeFileSync(htmlPath, html)
     let svg = await renderHtmlAndGetSvg(htmlPath)
     if (!existsSync(targetDir)) {
       mkdirSync(targetDir)
     }
     await writeFileSync(`${targetDir}/${urlHash}.svg`, svg)
-    await unlinkSync(htmlPath)
+    if (!env.keepHtml) {
+      await unlinkSync(htmlPath)
+    }
     res.sendFile(resolve(`${targetDir}/${urlHash}.svg`))
   })
 }
 
 const hbsTemplateToHtml = async (
   templatePath,
-  data = {},
-  storageDir
+  data = {}
 ) => {
   let source = await readFileSync(templatePath).toString()
   let compiler = handlebars.compile(source)
   let html = compiler({ data: JSON.stringify({...data}) })
-  let name = new Date().getUTCMilliseconds()
-  await writeFileSync(`${storageDir}/${name}.html`, html)
-  return resolve(`${storageDir}/${name}.html`)
+  return html
 }
 
 const renderHtmlAndGetSvg = async (
   htmlPath
 ) => {
   const browser = await puppeteer.launch({
-    args: ['--enable-font-antialiasing']
+    headless: false,
+    deviceScaleFactor: 2
   })
   const page = await browser.newPage()
   page.setViewport({ width: 1000, height: 1000 })
   await page.goto(`file://${htmlPath}`, { waitUntil: 'networkidle0' })
-  await page.waitForSelector('#badge')
+  await page.waitForSelector('#done')
   const svg = await page.$eval('#badge', el => el.innerHTML)
   await browser.close()
   return svg
