8fcf9bd0abefaf83b23abf0ec956d24e9e8225ce
Matteo Maronati <matteo.maronati@flixbus.com>
2018-08-17 02:41:41 +0200

    Add: bau utils -> google font as baseencode

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { resolve } = require('path')
-const { woof } = require('./routes')
+const { woof, bau } = require('./routes')
 const express = require('express')
 const app = express()
 
 const badgeFolder = resolve('./badges')
 const bark = async () => {
   woof(app, badgeFolder)
+  bau(app)
   app.use(express.static('./public'))
   app.listen(3100)
 }
