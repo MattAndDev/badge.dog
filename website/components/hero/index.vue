1a84a8d8bb4a84e685eae863a0c91c5deab7fc1b
Matteo Maronati <matteo.maronati@flixbus.com>
2018-08-18 23:27:51 +0200

    Website: give it some style (init hero)

--- website/components/hero/index.vue
+++ website/components/hero/index.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="hero">
-    <div class="hero_centerd">
+    <div class="hero_center">
       <h1 class="hero_title">badge.dog</h1>
+      <p class="hero_ups">All the badges you need</p>
+      <img src="https://badge.dog/woof/simple.svg?leftText=README&rightText=APPROVED&googleFontName=IBM+Plex+Sans&leftBgColor=%236C5B7D&rightBgColor=wheat&rightTextColor=%236C5B7D" alt="">
+      <span class="hero_arrow"></span>
     </div>
   </div>
 </template>
-<script src="./hero.js">
-  
-</script>
+<script src="./hero.js"></script>
 <style lang="sass">
 @import 'hero'
 </style>
