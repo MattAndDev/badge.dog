afb117b1a52c02952ed1517a43a8e29439e5e848
Matteo Maronati <matteo.maronati@flixbus.com>
2018-08-18 19:14:52 +0200

    Website: add website:dev script vua webpack-serve ready to actually do some FE #5

--- website/app.vue
+++ website/app.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="#App">
-    <a style="text-decoration: none; color: #333;" href="https://github.com/MattAndDev/badge.dog">  
-      <h1 style="font-family: sans-serif;text-align: center">badge.dog 🏅 🐶</h1>
-    </a> 
+   <hero />
   </div>
 </template>
+<script>
+import Hero from './components/hero'
+export default {
+  name: 'app',
+  components: {
+   Hero,
+  }
+}
+</script>
