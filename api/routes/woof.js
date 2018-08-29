70a6309ccc2458fc44970c88385d119ff692f641
Matteo Maronati <matteo.maronati@flixbus.com>
2018-08-29 20:30:05 +0200

    Api: move hash to service, improve woof endpoint

--- api/routes/woof.js
+++ api/routes/woof.js
@@ -1,31 +1,38 @@
-const { readFileSync, writeFileSync, unlinkSync, existsSync, mkdirSync } = require('fs')
-const { resolve, dirname } = require('path')
+const { readFileSync, writeFileSync, unlinkSync, existsSync, mkdirSync, copyFileSync } = require('fs')
+const { resolve } = require('path')
 const { renderFile } = require('../services/chromium')
+const { fromString } = require('../services/hash')
 
 const woof = (app, storageDir) => {
   app.get('/woof/:template.svg', async function (req, res) {
-    let templatePath = resolve(`./api/templates/${req.params.template}/index.html`)
+    let urlHash = await fromString(req.url)
+    let templateDir = resolve(`./api/templates/${req.params.template}/`)
+    let targetStorageDir = resolve(`${storageDir}/${req.params.template}`)
+    let templatePath = `${templateDir}/index.html`
+    let htmlTempPath = `${templateDir}/${urlHash}.html`
+    let htmlTargetPath = `${targetStorageDir}/${urlHash}.html`
+    let svgPath = `${targetStorageDir}/${urlHash}.svg`
     if (!await existsSync(templatePath)) {
       res.status(404)
       res.send('Template no found')
       return false
     }
-    let urlHash = await generateHash(req.url)
-    let targetDir = `${storageDir}/${req.params.template}`
-    if (!existsSync(targetDir)) {
-      mkdirSync(targetDir)
+    if (!existsSync(targetStorageDir)) {
+      mkdirSync(targetStorageDir)
     }
-    if (await existsSync(`${targetDir}/${urlHash}.svg`) && process.env.DEBUG === 'false') {
-      res.sendFile(resolve(`${targetDir}/${urlHash}.svg`))
+    if (await existsSync(svgPath) && process.env.DEBUG === 'false') {
+      res.sendFile(svgPath)
       return false
     }
     let html = await addQueryToTemplate(templatePath, req.query)
-    let htmlPath = `${dirname(templatePath)}/${urlHash}.html`
-    await writeFileSync(htmlPath, html)
-    let svg = await renderFile(htmlPath, '#done', '#badge')
-    await writeFileSync(`${targetDir}/${urlHash}.svg`, svg)
-    await unlinkSync(htmlPath)
-    res.sendFile(resolve(`${targetDir}/${urlHash}.svg`))
+    await writeFileSync(htmlTempPath, html)
+    let svg = await renderFile(htmlTempPath, '#done', '#badge')
+    await writeFileSync(svgPath, svg)
+    if (process.env.DEBUG === 'true') {
+      await copyFileSync(htmlTempPath, htmlTargetPath)
+    }
+    await unlinkSync(htmlTempPath)
+    res.sendFile(resolve(svgPath))
   })
 }
 
@@ -39,15 +46,6 @@ const addQueryToTemplate = async (
       const query = ${JSON.stringify({...query})}
     </script>`.replace(/^ {4}/gm, '')
   return `${js}\n${source}`
-}
-
-const generateHash = async (str) => {
-  let hash = 5381
-  let length = str.length
-  while (length) {
-    hash = (hash * 33) ^ str.charCodeAt(--length)
-  }
-  return hash >>> 0
 }
 
 module.exports = woof
