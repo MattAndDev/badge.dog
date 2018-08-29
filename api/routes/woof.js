cded59b12a66a9d40f8c1106c773f18889d8f698
Matteo Maronati <matteo.maronati@flixbus.com>
2018-08-30 00:23:09 +0200

    Api: add basic png support for badges rel #6

--- api/routes/woof.js
+++ api/routes/woof.js
@@ -1,38 +1,40 @@
-const { readFileSync, writeFileSync, unlinkSync, existsSync, mkdirSync, copyFileSync } = require('fs')
+const { readFileSync, writeFileSync, unlinkSync, existsSync, mkdirSync } = require('fs')
 const { resolve } = require('path')
 const { renderFile } = require('../services/chromium')
 const { fromString } = require('../services/hash')
 
 const woof = (app, storageDir) => {
-  app.get('/woof/:template.svg', async function (req, res) {
-    let urlHash = await fromString(req.url)
+  app.get('/woof/:template.:ext', async function (req, res) {
+    let id = await fromString(req.url)
     let templateDir = resolve(`./api/templates/${req.params.template}/`)
     let targetStorageDir = resolve(`${storageDir}/${req.params.template}`)
     let templatePath = `${templateDir}/index.html`
-    let htmlTempPath = `${templateDir}/${urlHash}.html`
-    let htmlTargetPath = `${targetStorageDir}/${urlHash}.html`
-    let svgPath = `${targetStorageDir}/${urlHash}.svg`
+    let htmlTempPath = `${templateDir}/${id}.html`
+    let svgPath = `${targetStorageDir}/${id}.svg`
+    let pngPath = `${targetStorageDir}/${id}.png`
+    let reqFile = (req.params.ext === 'svg') ? svgPath : pngPath
     if (!await existsSync(templatePath)) {
       res.status(404)
       res.send('Template no found')
       return false
     }
+
+    if (await existsSync(reqFile) && process.env.DEBUG === 'false') {
+      res.sendFile(reqFile)
+      return false
+    }
+
+    let html = await addQueryToTemplate(templatePath, req.query)
+    await writeFileSync(htmlTempPath, html)
+    let {string, screenshot} = await renderFile(htmlTempPath, '#done', 'svg', true)
+
     if (!existsSync(targetStorageDir)) {
       mkdirSync(targetStorageDir)
     }
-    if (await existsSync(svgPath) && process.env.DEBUG === 'false') {
-      res.sendFile(svgPath)
-      return false
-    }
-    let html = await addQueryToTemplate(templatePath, req.query)
-    await writeFileSync(htmlTempPath, html)
-    let svg = await renderFile(htmlTempPath, '#done', '#badge')
-    await writeFileSync(svgPath, svg)
-    if (process.env.DEBUG === 'true') {
-      await copyFileSync(htmlTempPath, htmlTargetPath)
-    }
+    await writeFileSync(svgPath, string)
+    await writeFileSync(pngPath, screenshot)
     await unlinkSync(htmlTempPath)
-    res.sendFile(resolve(svgPath))
+    res.sendFile(reqFile)
   })
 }
 
--- api/services/chromium.js
+++ api/services/chromium.js
@@ -10,28 +10,35 @@ const puppeteer = require('puppeteer')
 *  @param {String} htmlPath - absolute path of html file to render
 *  @param {String} [waitForSelector = false] - DOM selector to wait for
 *  @param {String} [returnSelector = false] - DOM node to return instead of complete page
-*  @returns {String} the resulting html as string
+*  @returns {Object} {html, screenshot} the resulting html as string and a png screenshot as buffer
 */
 
 const renderFile = async (
   htmlPath,
   waitForSelector = false,
-  returnSelector = false
+  returnSelector = false,
+  takeScreenshot = true,
+  size = { width: 1000, height: 1000 }
 ) => {
-  let browserOpt = (process.env.NODE_ENV === 'development') ? {dumpio: true, headless: process.env.HEADLESS} : {}
+  let browserOpt = (process.env.NODE_ENV === 'development') ? {deviceScaleFactor: 2, dumpio: true, headless: (process.env.HEADLESS === 'true')} : {}
   let browser = await puppeteer.launch(browserOpt)
   let page = await browser.newPage()
   if (process.env.NODE_ENV === 'development') {
     page.on('console', msg => console.log('PAGE LOG:', msg.text()))
   }
-  page.setViewport({ width: 1000, height: 1000 })
+  page.setViewport(size)
   await page.goto(`file://${htmlPath}`, { waitUntil: 'networkidle0' })
   if (waitForSelector) {
     await page.waitForSelector(waitForSelector)
   }
-  let result = (returnSelector) ? await page.$eval(returnSelector, el => el.innerHTML) : await page.content()
+  let string = (returnSelector) ? await page.$eval(returnSelector, el => el.outerHTML) : await page.content()
+  let screenshot = false
+  if (takeScreenshot) {
+    let screenshotHandle = (returnSelector) ? await page.$(returnSelector) : page
+    screenshot = await screenshotHandle.screenshot({omitBackground: true})
+  }
   await browser.close()
-  return result
+  return {string, screenshot}
 }
 
 module.exports = {
