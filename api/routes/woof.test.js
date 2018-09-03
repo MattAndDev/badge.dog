91b7961b7157b1b271ae21d3594117530f8f416d
Matteo Maronati <matteo.maronati@flixbus.com>
2018-09-04 00:59:20 +0200

    Env: improve env var usage

---
+++ api/routes/woof.test.js
@@ -0,0 +1,32 @@
+/* global env, expect, test, describe */
+const { resolve } = require('path')
+const exec = require('child_process').exec
+const { mkdirSync, existsSync } = require('fs')
+const woof = require('./woof')
+const utils = require('./utils')
+const express = require('express')
+const fetch = require('node-fetch')
+const app = express()
+const storageFolder = resolve('./test')
+
+process.env = {...process.env, ...env}
+
+const bark = async () => {
+  if (!await existsSync(storageFolder)) {
+    await mkdirSync(storageFolder)
+  }
+  app.use('/templates', express.static('./templates'))
+  woof(app, storageFolder)
+  utils(app)
+  return app.listen(process.env.PORT)
+}
+
+describe('/woof endpoint', async () => {
+  test('test default templates', async () => {
+    const app = await bark()
+    let res = await fetch(`http://localhost:${process.env.PORT}/woof/simple.svg`)
+    app.close()
+    expect(res.status).toEqual(200)
+    exec('rm -rf ./test')
+  })
+})
--- api/services/chromium.js
+++ api/services/chromium.js
@@ -3,31 +3,31 @@
 const puppeteer = require('puppeteer')
 
 /**
-*  render html file with puppeteer and returns dom as string
+*  render url with puppeteer and returns dom as string
 *  optionally
 *
-*  @func renderFile
-*  @param {String} htmlPath - absolute path of html file to render
+*  @func renderUrl
+*  @param {String} url - url to render
 *  @param {String} [waitForSelector = false] - DOM selector to wait for
 *  @param {String} [returnSelector = false] - DOM node to return instead of complete page
 *  @returns {Object} {html, screenshot} the resulting html as string and a png screenshot as buffer
 */
 
-const renderFile = async (
-  htmlPath,
+const renderUrl = async (
+  url,
   waitForSelector = false,
   returnSelector = false,
   takeScreenshot = true,
   size = { width: 1000, height: 1000 }
 ) => {
-  let browserOpt = (process.env.NODE_ENV === 'development') ? {deviceScaleFactor: 2, dumpio: true, headless: (process.env.HEADLESS === 'true')} : {}
+  let browserOpt = process.env.PUPPETEER || {}
   let browser = await puppeteer.launch(browserOpt)
   let page = await browser.newPage()
   if (process.env.NODE_ENV === 'development') {
     page.on('console', msg => console.log('PAGE LOG:', msg.text()))
   }
   page.setViewport(size)
-  await page.goto(`file://${htmlPath}`, { waitUntil: 'networkidle0' })
+  await page.goto(url, { waitUntil: 'networkidle0' })
   if (waitForSelector) {
     await page.waitForSelector(waitForSelector)
   }
@@ -42,5 +42,5 @@ const renderFile = async (
 }
 
 module.exports = {
-  renderFile
+  renderUrl
 }
